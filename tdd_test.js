f2b9274b49f095a86f680727c1b10bfef09f9f30
sisonke <culoe2@gmail.com>
2015-06-15 10:39:32 +0200

    Delete tdd_test.js

--- tdd_test.js
+++
@@ -1,6 +0,0 @@
-TestMyCode.run("testing hello world function", function(assert){
-    var result = helloWorld();
-    // is the result as we expected?
-    assert.equals("hello world!", result);
-});
-
